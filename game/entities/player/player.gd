d1b1490c0c8ab7f370820e75b227cdb424661fef
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-02-04 15:14:42 -0600

    Add idle animation and flipping sprite on direction

--- game/entities/player/player.gd
+++ game/entities/player/player.gd
@@ -1,15 +1,29 @@
 extends CharacterBody2D
 
+
+## Initialization ##
+@onready var animation : AnimationPlayer = $AnimationPlayer
+@onready var sprite : Sprite2D = $Sprite2D
+
+
 ## Constants ##
 const SPEED = 200.0
 const JUMP_HEIGHT = -350.0
+
 
 ## Instance variables ##
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
 ## Engine functions ##
 func _physics_process(delta):
+	# Handle flipping sprite
+	if Input.is_action_pressed("left"):
+		sprite.scale.x = abs(sprite.scale.x) * -1
+	elif Input.is_action_pressed("right"):
+		sprite.scale.x = abs(sprite.scale.x)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -24,6 +38,7 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
+		animation.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
