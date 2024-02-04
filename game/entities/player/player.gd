c4587485fa638b441a1d67ec39f60a58957a1fbe
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-02-04 17:04:52 -0600

    Update and add animations for player

--- game/entities/player/player.gd
+++ game/entities/player/player.gd
@@ -11,9 +11,13 @@ const SPEED = 200.0
 const JUMP_HEIGHT = -350.0
 
 
-## Instance variables ##
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+## Variables ##
+# Get the gravity from the project settings to be synced with RigidBody nodes
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _ready():
+	animation.play("Idle")
 
 
 ## Engine functions ##
@@ -24,16 +28,15 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("right"):
 		sprite.scale.x = abs(sprite.scale.x)
 	
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_HEIGHT
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement/deceleration
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -44,4 +47,4 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-## Instance functions ##
+## Custom functions ##
