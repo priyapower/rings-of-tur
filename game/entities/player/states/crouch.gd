575beee7f2754616fc9c0647262db712f0a67661
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-02-05 10:45:06 -0600

    Add pseudocode and input mapping

--- game/entities/player/states/crouch.gd
+++ game/entities/player/states/crouch.gd
@@ -1,2 +1,36 @@
 # Crouching state
 extends State
+
+# Update input mapping for crouch
+	# key: Down
+	# key: S
+
+# [External files] States that transition to crouch
+	# These scripts need update (if on_floor && input == crouch: run_crouch)
+		# Idle --> crouch
+		# Run --> crouch
+		# Fall --> crouch
+
+# States that this script handles (transtions from crouch)
+	# crouch --> Idle
+	# crouch --> Run
+	# crouch --> Jump
+@export var idle_state: State
+@export var run_state: State
+@export var jump_state: State
+
+
+func process_input(event: InputEvent) -> State:
+	# Handle sprite direction
+	if event.is_action_pressed("left"):
+		parent.sprite.scale.x = abs(parent.sprite.scale.x) * -1
+	if event.is_action_pressed("right"):
+		parent.sprite.scale.x = abs(parent.sprite.scale.x)
+
+	return null
+
+func process_physics(delta: float) -> State:
+	# Gravity?
+	# Handles horizontal movement
+	# Speed shouldn't be the same as run speed
+	return null
