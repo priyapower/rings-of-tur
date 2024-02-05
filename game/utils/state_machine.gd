4905468fb2af02820140537fe4d1f8a135b912e3
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-02-05 10:13:28 -0600

    :broom:

--- game/utils/state_machine.gd
+++ game/utils/state_machine.gd
@@ -7,7 +7,6 @@ var current_state: State
 # Initialize the state machine by giving each child state a reference to the
 # parent object it belongs to and enter the default starting_state.
 func init(parent: Player) -> void:
-	print("parent: ", parent)
 	for child in get_children():
 		child.parent = parent
 
@@ -19,7 +18,6 @@ func change_state(new_state: State) -> void:
 	if current_state:
 		current_state.exit()
 
-	print("new_state: ", new_state)
 	current_state = new_state
 	current_state.enter()
 	
